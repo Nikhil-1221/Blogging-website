18b34e2663f692e904a4123ffbc58e03358b41f3
Nikhil_1221 <Nikhilkumarsinghpbic@gmail.com>
2024-07-25 23:51:45 +0530

    Blogging Website

---
+++ blogging-website-backend/config/allowedOrigins.js
@@ -0,0 +1,9 @@
+
+const allowedOrigins = [
+    'http://localhost:5173',
+    'http://localhost:4200',
+    'https://blogging-website-frontend-theta.vercel.app/'
+
+];
+
+module.exports = allowedOrigins;
